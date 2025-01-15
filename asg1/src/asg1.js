8ea56a302a04fd1cd43abd785141ac9edd1eb728
proudnhello <92887967+proudnhello@users.noreply.github.com>
2025-01-14 22:36:41 -0800

    whoops forgor to commit to git
    Finished through 1.7, also added a color picker

--- asg1/src/asg1.js
+++ asg1/src/asg1.js
@@ -1,95 +1,163 @@
 // ColoredPoint.js (c) 2012 matsuda
 // Vertex shader program
 var VSHADER_SOURCE =
-  'attribute vec4 a_Position;\n' +
-  'void main() {\n' +
-  '  gl_Position = a_Position;\n' +
-  '  gl_PointSize = 10.0;\n' +
-  '}\n';
+    `attribute vec4 a_Position;
+    uniform float u_PointSize;
+    void main() {
+        gl_Position = a_Position;
+        gl_PointSize = u_PointSize;
+    }`;
 
 // Fragment shader program
 var FSHADER_SOURCE =
-  'precision mediump float;\n' +
-  'uniform vec4 u_FragColor;\n' +  // uniform変数
-  'void main() {\n' +
-  '  gl_FragColor = u_FragColor;\n' +
-  '}\n';
+    `precision mediump float;
+    uniform vec4 u_FragColor;
+    void main() {
+        gl_FragColor = u_FragColor;
+    }`;
 
-function main() {
-  // Retrieve <canvas> element
-  var canvas = document.getElementById('webgl');
+let canvas;
+let gl;
+let a_Position;
+let u_PointSize;
+let u_FragColor;
+let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
+let u_selectedSize = 10;
+let g_shapes = [];
 
-  // Get the rendering context for WebGL
-  var gl = getWebGLContext(canvas);
-  if (!gl) {
-    console.log('Failed to get the rendering context for WebGL');
-    return;
-  }
+function setupWebGL() {
+    // Retrieve <canvas> element
+    canvas = document.getElementById('webgl');
 
-  // Initialize shaders
-  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to intialize shaders.');
-    return;
-  }
-
-  // // Get the storage location of a_Position
-  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  if (a_Position < 0) {
-    console.log('Failed to get the storage location of a_Position');
-    return;
-  }
-
-  // Get the storage location of u_FragColor
-  var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
-  if (!u_FragColor) {
-    console.log('Failed to get the storage location of u_FragColor');
-    return;
-  }
-
-  // Register function (event handler) to be called on a mouse press
-  canvas.onmousedown = function(ev){ click(ev, gl, canvas, a_Position, u_FragColor) };
-
-  // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
+    // Get the rendering context for WebGL
+    gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
 }
 
-var g_points = [];  // The array for the position of a mouse press
-var g_colors = [];  // The array to store the color of a point
-function click(ev, gl, canvas, a_Position, u_FragColor) {
-  var x = ev.clientX; // x coordinate of a mouse pointer
-  var y = ev.clientY; // y coordinate of a mouse pointer
-  var rect = ev.target.getBoundingClientRect();
+function connectVariablesToGLSL() {
+    // Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
 
-  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
-  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+    // Get the storage location of a_Position
+    a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
 
-  // Store the coordinates to g_points array
-  g_points.push([x, y]);
-  // Store the coordinates to g_points array
-  if (x >= 0.0 && y >= 0.0) {      // First quadrant
-    g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
-  } else if (x < 0.0 && y < 0.0) { // Third quadrant
-    g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
-  } else {                         // Others
-    g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
-  }
+    // Get the storage location of u_PointSize
+    u_PointSize = gl.getUniformLocation(gl.program, 'u_PointSize');
+    if (u_PointSize < 0) {
+        console.log('Failed to get the storage location of u_PointSize');
+        return;
+    }
 
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
+    // Get the storage location of u_FragColor
+    u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    if (!u_FragColor) {
+        console.log('Failed to get the storage location of u_FragColor');
+        return;
+    }
+}
 
-  var len = g_points.length;
-  for(var i = 0; i < len; i++) {
-    var xy = g_points[i];
-    var rgba = g_colors[i];
+// This hex function heavily based on the one from https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+// There didn't seem to be a built-in function in JavaScript to convert hex to rgb, and I do not like regex, so I found this
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    let r = result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16),
+    } : null;
+    if (r == null) {
+        console.log('Failed to convert hex to rgb');
+        return [1.0, 1.0, 1.0, 1.0];
+    }
+    return [r.r / 255.0, r.g / 255.0, r.b / 255.0, 1.0];
+}
+// Copied function ends here
 
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-    // Pass the color of a point to u_FragColor variable
-    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-    // Draw
-    gl.drawArrays(gl.POINTS, 0, 1);
-  }
+function addActionsForHtmlUI() {
+    // Button events
+    document.getElementById('clear').onclick = function() {
+        g_shapes = [];
+        renderAllShapes();
+    };
+
+    // Slider events 
+    document.getElementById('red').addEventListener('mouseup', function() {
+        g_selectedColor[0] = this.value;
+    });
+    document.getElementById('green').addEventListener('mouseup', function() {
+        g_selectedColor[1] = this.value;
+    });
+    document.getElementById('blue').addEventListener('mouseup', function() {
+        g_selectedColor[2] = this.value;
+    });
+
+    document.getElementById('size').addEventListener('mouseup', function() {
+        u_selectedSize = this.value;
+    });
+
+    // Color picker event
+    document.getElementById('color').addEventListener("input", function() {
+        g_selectedColor = hexToRgb(this.value);
+    });
+}
+
+function convertCoordinates(ev) {
+    const x = ev.clientX; // x coordinate of a mouse pointer
+    const y = ev.clientY; // y coordinate of a mouse pointer
+    const rect = ev.target.getBoundingClientRect();
+
+    return {
+        x: ((x - rect.left) - canvas.width/2)/(canvas.width/2),
+        y: (canvas.height/2 - (y - rect.top))/(canvas.height/2)
+    };
+}
+
+function main() {
+    setupWebGL();
+
+    connectVariablesToGLSL();
+
+    addActionsForHtmlUI();
+
+    // Register function (event handler) to be called on a mouse press
+    canvas.onmousedown = click;
+
+    // Specify the color for clearing <canvas>
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+function click(ev) {
+    [x, y] = [convertCoordinates(ev).x, convertCoordinates(ev).y];
+
+    // Store the coordinates to g_points array
+    g_shapes.push(new Point([x, y], g_selectedColor, u_selectedSize));
+
+    renderAllShapes();
+}
+
+function drawPoint(shape) {
+    
+}
+
+function renderAllShapes() {
+    // Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    var len = g_shapes.length;
+    for(var i = 0; i < len; i++) {
+        g_shapes[i].render();
+    }
 }
